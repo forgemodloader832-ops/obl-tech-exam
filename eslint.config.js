1377fb8425f0f95d6d48f35699e0f33868781731
forgemodloader832-ops <forgemodloader832@gmail.com>
2026-04-24 16:24:22 +0900

    lint fix
    lint fix

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
@@ -10,5 +9,12 @@ export default defineConfig([
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
-  globalIgnores([".config/*"])
+  globalIgnores(["**/*.js", "**/*.cjs", "**/*.mjs", "**/*.d.ts"]),
+	{
+		files: ["**/*.ts"],
+		ignores: [".config/**"],
+		rules: {
+			/* ... */
+		},
+	},
 ]);
